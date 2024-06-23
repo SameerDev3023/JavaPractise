7cc910950ced0e2af4138615df846fb35c8b1bfa
sameer <sameerdeveloper05@gmail.com>
2024-06-23 10:57:38 +0530

    ways of declare variables

--- Variables.java
+++ Variables.java
@@ -2,7 +2,23 @@ public class Variables {
     public static void main(String args[]){
 
         String Name = "My name is mohammad sameer";
-        System.out.println(Name);
+        char character  ='A';
+        int myNum = 4;
+        float floatNum = 303.3f; 
+        double doubleNum = 3.3;
+        boolean booleanVal = true;
+
+        //  Same Type Variable we can declare like this with comma seprated ...
+
+        int numOne = 1, numTwo = 2, numThree = 3;
+        char charOne = 'A',charTwo = 'B', charThree = 'C';
+        String stringOne = "Hello", stringTwo = "world",stringThree = "Hello World";
+        double doubleOne = 23.3 , doubleTwo = 53.43, doubleThree = 343.34;
+        float FloatOne = 434.43f , FloatTwo = 34234.453f , FloatThree = 34324.3243f;
+        boolean  booleanOne = true, booleanTwo = false;
+
+        System.out.println(booleanVal);
+
 
     }
 }
