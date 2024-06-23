e747afdfd5167dc3a4cf4d034b14bed1f8fbac4c
sameer <sameerdeveloper05@gmail.com>
2024-06-23 15:15:14 +0530

    Recursion Fibonacci Series

--- Recursion.java
+++ Recursion.java
@@ -8,9 +8,32 @@ class Recursion{
         return (n<0)?0: n + SumofAllNumbers(n-1);
     }
     
+
+    public static void FibonacciSeries(int a, int b,int n){
+        if(n==0){
+            return;
+        } 
+        int c = a+b;
+        System.out.println(c);
+        FibonacciSeries(b,c,n-1);
+    }
+
+    public static void PrintNumbers(int n){
+      if(n==0){
+        return;
+      }else{
+          PrintNumbers(n-1);
+          System.out.println(n);
+      }
+     
+    }
     public static void main(String[] args){
         System.out.println(Factorial(5));
         System.out.println(SumofAllNumbers(3));
+        PrintNumbers(10);
+        FibonacciSeries(0,1,7);
+
+
        
     }
 
