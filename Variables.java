5ac77a902974232e4308c5ba4f0840445f1027c7
sameer <sameerdeveloper05@gmail.com>
2024-06-23 11:31:15 +0530

    Add comments in the code

--- Variables.java
+++ Variables.java
@@ -1,18 +1,27 @@
 public class Variables {
     public static void main(String args[]){
 
-        String Name = "My name is mohammad sameer";
-        char character  ='A';
-        int myNum = 4;
-        float floatNum = 303.3f; 
-        double doubleNum = 3.3;
-        boolean booleanVal = true;
+        String Name = "My name is mohammad sameer"; //String
+
+        // Primitive Data Type mentioned below
+        byte byteOne = 127; // Store Whole number from -128 to 127  SIZE :- 1 BYTE
+        boolean booleanVal =true; // Store only boolean values like true or false. SIZE :- 1 BYTE
+        short shortOne = 32767;// Store whole numner from -32767 to 32767; SIZE :- 2 BYTES
+        char character  ='A'; // Store All characters in single qoutes 'A','B','C' SIZE :- 2 BYTES
+        int myNum = 4;// Store Whole numbers like 1,2,3,4,5,6,-1,-2,34 so on..  SIZE :- 4 BYTES
+        float floatNum = 303.3f;  // Store whole float numbers like 343.34f ,343543.34f or so on... SIZE 4 BYTES
+        long LongOne = 3434345435435453943L; // SIZE :- 8 BYTES
+        double doubleNum = 3.3; // Store whole double numbers with decimals like 34.43,4345.3 or so on. SIZE :- 8 BYTES
+       
+        
+      
 
         //  Same Type Variable we can declare like this with comma seprated ...
-
-        int numOne = 1, numTwo = 2, numThree = 3;
+        
+        int numOne = 343455551, numTwo = 2, numThree = 3;
         char charOne = 'A',charTwo = 'B', charThree = 'C';
         String stringOne = "Hello", stringTwo = "world",stringThree = "Hello World";
+        long LongNum = 922343807;
         double doubleOne = 23.3 , doubleTwo = 53.43, doubleThree = 343.34;
         float FloatOne = 434.43f , FloatTwo = 34234.453f , FloatThree = 34324.3243f;
         boolean  booleanOne = true, booleanTwo = false;
